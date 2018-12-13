6c41c871a3a37520ad7346389903b00b6f78d6bf
kevin chan <kevinwochan@gmail.com>
2018-12-13 22:46:56 +1100

    2018 start

--- 2017/advent3.c
+++ 2017/advent3.c
@@ -1,41 +1,46 @@
-set i 31 i = 31
-set a 1  a = 1
-mul p 17 p = 0
-jgz p p  if ( p > 0) jump to p(0)
-mul a 2  a = 2; a = 4 ; a = 8; .. a = 2^30 OR 31  
-add i -1 i = 30; i = 29; i = 28 ... i = 0
-jgz i -2 if ( i > 0 ) jump up 2 lines  
-add a -1 2^30 - 1 
-set i 127 i = 127
-set p 735 p = 735
-mul p 8505  p = 735 * 8505
-mod p a   p = 735 * 8505 * (2^30 - 1)
-mul p 129749 p = 735 * 8505 * (2^30 - 1) * 129749
-add p 12345 p = 12345 + 735 * 8505 * (2^30 - 1) * 129749
-mod p a   p = 12345 + 735 * 8505 * (2^30 - 1) * 129749% (2^30 - 1)
-set b p   b = 12345 + 735 * 8505 * (2^30 - 1) * 129749% (2^30 - 1)  
-mod b 10000 b = 
-snd b rcv = b 
-add i -1   i = 126
-jgz i -9   
-jgz a 3
-rcv b
-jgz b -1
-set f 0
-set i 126
-rcv a
-rcv b
-set p a
-mul p -1
-add p b
-jgz p 4
-snd a
-set a b
-jgz 1 3
-snd b
-set f 1
-add i -1
-jgz i -11
-snd a
-jgz f -16
-jgz a -19
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+/*
+--- Day 3: Spiral Memory ---
+You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
+
+Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
+
+17  16  15  14  13
+18   5   4   3  12
+19   6   1   2  11
+20   7   8   9  10
+21  22  23---> ...
+While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
+
+For example:
+
+Data from square 1 is carried 0 steps, since it's at the access port.
+Data from square 12 is carried 3 steps, such as: down, left, left.
+Data from square 23 is carried only 2 steps: up twice.
+Data from square 1024 must be carried 31 steps.
+How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
+
+*/
+    //first frame size  = 1 1 squared
+    //second frame size = 9 3 squared
+    //thirdframe = 25 5 squared
+    //
+int valueAt( int x, int y ){
+    if ( x == 0 ) return 1;
+    return x*x + valueAt( x--, y );
+}
+
+int main (){
+    int input = 265149;
+    int inputX = 0, inputY = 0;
+    
+    // find the vector to the input
+    int x = 0, y = 0;
+    while (inputX == 0 && inputY == 0){
+        int curr = valueAt(x++,y++);
+        if ( curr == input ) {inputX = x; inputY = y; break;}
+    }
+    return 1;
+}
